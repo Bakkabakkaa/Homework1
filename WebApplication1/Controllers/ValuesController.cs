ef4c0951676063ec659127a0d5f19ef64297976e
Art'm <kasko.artem45@gmail.com>
2025-12-24 03:57:16 +0300

    add ValuesController

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("controller")]
+public class ValuesController : Controller
+{
+    [HttpGet("string")]
+    public string GetString(string value = "Ni Hao")
+    {
+        return value;
+    }
+
+    [HttpGet("int")]
+    public int GetInt(int value)
+    {
+        return value;
+    }
+
+    [HttpGet("float")]
+    public float GetFloat()
+    {
+        return 3.14f;
+    }
+
+    [HttpGet("datetime")]
+    public DateTime GetDateTime()
+    {
+        return DateTime.Now;
+    }
+    
+    [HttpGet("array")]
+    public string[] GetArray()
+    {
+        var array = new string[] { "Someday", "Somewhere", "Sure" };
+
+        return array;
+
+    }
+}
